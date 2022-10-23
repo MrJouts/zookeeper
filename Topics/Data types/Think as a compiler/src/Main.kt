bbfcf102b331ecbdeb1c4df77d139dfdc2dc0b9d
Emiliano <emiliano.hotes@despegar.com>
2022-10-23 19:57:42 -0300

    Data types - finished

---
+++ Topics/Data types/Think as a compiler/src/Main.kt
@@ -0,0 +1,7 @@
+fun main() {
+    val first: String = "Emi"
+    val last: String = "Jouts"
+    val age: Int = 36
+    // put your code above
+    println("My name is $first $last and I’m $age years old")
+}
