412041ebe47d1c8334f266cfce9987440f5079dd
Emiliano <emiliano.hotes@despegar.com>
2022-11-02 20:06:05 -0300

    Introduction to MutableList

---
+++ Topics/Introduction to MutableList/Swap/src/Main.kt
@@ -0,0 +1,10 @@
+fun main() {    
+    val numbers = readLine()!!.split(' ').map { it.toInt() }.toMutableList()
+    // Do not touch lines above
+    // Write only exchange actions here.
+    val first = numbers[0]
+    numbers[0] = numbers[numbers.size - 1]
+    numbers[numbers.size - 1] = first
+    // Do not touch lines below
+    println(numbers.joinToString(separator = " "))
+}
